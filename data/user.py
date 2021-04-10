1b5ecefd09d6a4a8fab7c974ce3fc654fbb06faa
SH1SH4 <dshisha0@gmail.com>
2021-04-10 22:24:31 +0300

    Быстрофикс таблицы

--- data/user.py
+++ data/user.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Text
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Text, Boolean
 from sqlalchemy.orm import validates, relationship
 from datetime import datetime
 from .db_session import SqlAlchemyBase
@@ -10,14 +10,15 @@ class User(SqlAlchemyBase):
     id = Column(Integer, primary_key=True, unique=True)
     mail = Column(String, nullable=False)
     password = Column(String, nullable=False)
-    table = relationship("tables")
+    table = relationship("homework")
 
 
 class Tables(SqlAlchemyBase):
-    __tablename__ = 'user'
+    __tablename__ = 'homework'
     id = Column(Integer, primary_key=True, unique=True)
     owner_id = Column(Integer, ForeignKey('users.id'))
     day = Column(DateTime)
     time = Column(DateTime)
     homework_text = Column(Text)
     homework_img = Column(String)
+    completed = Column(Boolean, default=False)
--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-from flask import app
+from flask import Flask
+from data import db_session
+from flask_restful import Api
+
+app = Flask(__name__)
+api = Api(app)
+
+
+
+def main():
+    app.run(host='0.0.0.0', port=8080, threaded=True)
+    db_session.global_init('db/homework.db')
+
+
+if __name__ == '__main__':
+    main()
