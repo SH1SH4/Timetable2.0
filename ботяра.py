08d157e1b29516fead31f88eba9b803cf63390f0
SashkaHacker <am942081@gmail.com>
2021-04-13 22:45:16 +0300

    bot telegram

--- ботяра.py
+++ ботяра.py
@@ -1,144 +1,99 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.ext import (
+    Updater,
+    CommandHandler,
+    MessageHandler,
+    Filters,
+    ConversationHandler,
+)
+from telegram import ReplyKeyboardMarkup, InlineKeyboardButton, InlineKeyboardMarkup
 import logging
 
+AUTORIZATION = range(1)
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
 
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+reply_keyboard = [
+    ["Расписание на завтра", "Записать ДЗ"],
+    ["Что на завтра задали",  "Расписание на неделю"]
+]
+markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True, resize_keyboard=True)
 
 
 def start(update, context):
     user = update.message.chat.id
-    if user in base:
-        update.message.reply_text("Вы авторизированный пользователь")
-    else:
-        update.message.reply_text("Пришлите токен для входа в аккаунт")
-        flag = True
-        while flag:
-            if update.message.text in base:
-                update.message.reply_text("Все успешно!")
-                flag = False
-            else:
-                update.message.reply_text("Ты позер, попробуй ещё раз!")
+    print(user)
+    if True:
+        update.message.reply_text('Выберите действие, для продолжения работы', reply_markup=markup)
 
-
-
-# def button(update, context):
-#     query = update.callback_query
-#     if query.data == 'pro':
-#         file = open(f'./Users/{query.message.chat.id}.txt', 'w')
-#         file.write(f'{query.data}')
-#         file.close()
-#         query.edit_message_text(text=f"Вы выбрали: {query.data} версию бота, теперь просто отправьте фото.")
-#     if query.data == 'lite':
-#         query.edit_message_text('Вы выбрали "LITE" версию бота,'
-#                                 '\nчтобы начать отправлять фото, выберите один из предложенных языков:'
-#                                 '\n/Russian - Русский'
-#                                 '\n/English - Английский'
-#                                 '\n/French - Французкий'
-#                                 '\n/German - Немецкий')
-
-
-# def language(update, context):
-#     keyboard = [[InlineKeyboardButton("LITE", callback_data='lite')],
-#                 [InlineKeyboardButton("PRO", callback_data='pro')]
-#                 ]
-#
-#     reply_markup = InlineKeyboardMarkup(keyboard)
-#     update.message.reply_text('Выберите версию:', reply_markup=reply_markup)
-
-
-# def language_lite(update, context):
-#     file_r = open(f'./Users/{update.message.chat.id}.txt', 'r')
-#     text = file_r.read()
-#     file_r.close()
-#     if text != 'pro':
-#         update.message.reply_text('Вы находитесь в "LITE" версии бота,'
-#                                     '\nчтобы изменить язык, выберите один из предложенных:'
-#                                     '\n/Russian - Русский'
-#                                     '\n/English - Английский'
-#                                     '\n/French - Французкий'
-#                                     '\n/German - Немецкий')
-#     else:
-#         update.message.reply_text('Смените режим бота на "LITE" версию, чтобы сменить язык.')
-
-#
-# def rus(update, context):
-#     file = open(f'./Users/{update.message.chat.id}.txt', 'w')
-#     file.write('rus')
-#     file.close()
-#     update.message.reply_text('Язык /Russian выбран, теперь отправьте фото текста на выбранном языке.')
-#
-#
-# def eng(update, context):
-#     file = open(f'./Users/{update.message.chat.id}.txt', 'w')
-#     file.write('eng')
-#     file.close()
-#     update.message.reply_text('Язык /English выбран, теперь отправьте фото текста на выбранном языке.')
-#
-#
-# def fra(update, context):
-#     file = open(f'./Users/{update.message.chat.id}.txt', 'w')
-#     file.write('fra')
-#     file.close()
-#     update.message.reply_text('Язык /French выбран, теперь отправьте фото текста на выбранном языке.')
-#
-#
-# def frk(update, context):
-#     file = open(f'./Users/{update.message.chat.id}.txt', 'w')
-#     file.write('frk')
-#     file.close()
-#     update.message.reply_text('Язык /German выбран, теперь отправьте фото текста на выбранном языке.')
-#
-
-def info(update, context):
-    update.message.reply_text('Бот создан для распознавания текcта с изображения, '
-                              'он может работать в двух режимах: "PRO" и "LITE", '
-                              'первый режим может максимально качественно распознать '
-                              'текcт, изображенный на картинке с помощью нейросетей от GOOGLE, '
-                              'но он будет платным. '
-                              'Второй же режим позволит вам распознать текст с картинки бесплатно, '
-                              'но не так качественно, как режим "PRO", так же для распознавания текста режимом "LITE" '
-                              'нужно сфотографировать текст максимально хорошо, на однородном фоне и при этом'
-                              'обрезав края. '
-                              'Чтобы запустить бота выполните команду /start и следуйте инструкциям. Так же вы можете '
-                              'воспользоваться командой /mods для изменения режима.')
-
-
-def photo(update, context):
-    # file_r = open(f'./Users/{update.message.chat.id}.txt', 'r')
-    # text = file_r.read()
-    # file_r.close()
-    file = update.message.photo[-1]
-    file_id = file.file_id
-    NewFile = context.bot.get_file(file_id)
-    NewFile.download(f'{file_id}.png')
-    update.message.reply_text(text_dectect.main(f'{file_id}.png'))
     # else:
-    #     update.message.reply_text(Photo.preob(update.message.chat.id, f'{file_id}.png'))
-    # update.message.reply_text('Для распознавания текста с новой фотографии просто отправьте её:).'
-    #                           '\nДля смены режима используйте /mods'
-    #                           '\nДля смены языка в "LITE" режиме используйте /language_lite')
-    # os.remove(f'./{file_id}.png')
+    # update.message.reply_text("Пришлите токен для входа в аккаунт")
+    # flag = True
+    # while flag:
+    #     text = update.message.text
+    #     if text in "База данных":
+    #         return AUTORIZATION
+    #         else:
+    #             update.message.reply_text("Ты позер, попробуй ещё раз!")
+
+
+def authorization(update, context):
+    update.message.reply_text("Регистрация пройдена успешно")
+    update.message.reply_text('Выберите действие, для продолжения работы', reply_markup=markup)
+
+
+def stop(update, context):
+    update.message.reply_text("всё плохо")
+
+
+def tomorrow(update, context):
+    update.message.reply_text("Раздел расписания на завтра")
+
+
+def homework(update, context):
+    update.message.reply_text("Раздел записи ДЗ")
+
+
+def tomorrow_homework(update, context):
+    schedule = [[InlineKeyboardButton(f"Английский", callback_data='1'), InlineKeyboardButton(f"Русский", callback_data='2')],
+                [InlineKeyboardButton(f"Физика", callback_data='3'), InlineKeyboardButton(f"Алгебра", callback_data='4')],
+                [InlineKeyboardButton(f"Геометрия", callback_data='5'), InlineKeyboardButton(f"Физ-ра", callback_data='6')]]
+    reply_keyboard = InlineKeyboardMarkup(schedule)
+
+    update.message.reply_text("Раздел ДЗ на завтра", reply_markup=reply_keyboard)
+
+
+def week(update, context):
+    schedule = [
+        [InlineKeyboardButton(f"Понедельник", callback_data='Monday'), InlineKeyboardButton(f"Вторник", callback_data='Tuesday')],
+        [InlineKeyboardButton(f"Среда", callback_data='Wednesday'), InlineKeyboardButton(f"Четверг", callback_data='Thursday')],
+        [InlineKeyboardButton(f"Пятница", callback_data='Friday'), InlineKeyboardButton(f"Суббота", callback_data='Saturday')]]
+    reply_keyboard = InlineKeyboardMarkup(schedule)
+
+    update.message.reply_text("Раздел расписания на неделю", reply_markup=reply_keyboard)
 
 
 def main():
     updater = Updater('1757297275:AAFWjozUO911jvNakuoeoSS8m1yZaA5txTY', use_context=True)
+    dispatcher = updater.dispatcher
 
-    dp = updater.dispatcher
+    conv_handler = ConversationHandler(
 
-    #
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("info", info))
-    # dp.add_handler(CallbackQueryHandler(button))
-    # dp.add_handler(CommandHandler("Russian", rus))
-    # dp.add_handler(CommandHandler("English", eng))
-    # dp.add_handler(CommandHandler("French", fra))
-    # dp.add_handler(CommandHandler("German", frk))
-    # dp.add_handler(CommandHandler("mods", language))
-    # dp.add_handler(CommandHandler("language_lite", language_lite))
+        entry_points=[CommandHandler('start', start)],
 
-    dp.add_handler(MessageHandler(Filters.photo, photo))
+        states={
+            # Добавили user_data для сохранения ответа.
+            AUTORIZATION: [MessageHandler(Filters.text, authorization, pass_user_data=True)]},
 
-    # Start the Bot
+        fallbacks=[MessageHandler(Filters.regex("^Stop&"), stop)]
+    )
+    # Команды бота
+    dispatcher.add_handler(MessageHandler(Filters.regex('^Расписание на завтра$'), tomorrow))
+    dispatcher.add_handler(MessageHandler(Filters.regex('^Записать ДЗ$'), homework))
+    dispatcher.add_handler(MessageHandler(Filters.regex('^Что на завтра задали$'), tomorrow_homework))
+    dispatcher.add_handler(MessageHandler(Filters.regex('^Расписание на неделю$'), week))
+    dispatcher.add_handler(conv_handler)
+    dispatcher.add_handler(CommandHandler("start", start))
+    # # Start the Bot
     updater.start_polling()
 
     updater.idle()
