615c81983f750f622bab524a392d993fa82e1295
SashkaHacker <am942081@gmail.com>
2021-04-14 22:18:18 +0300

    add login in telegram

--- modules/registration.py
+++ modules/registration.py
@@ -1,11 +1,13 @@
 from hashlib import sha256
 from tables.user import User
 from tables import db_session
+from secrets import token_urlsafe
 
 
 def reg(**kwargs):
     del kwargs['password_repeat']
     kwargs['password'] = sha256(kwargs['password'].encode('utf-8')).hexdigest()
+    kwargs['token'] = token_urlsafe(16)
     db_sess = db_session.create_session()
     print(kwargs)
     user = User(**kwargs)
