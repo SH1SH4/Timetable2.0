cdca559f49dacc0dafd0530ac2ebe94c10fd8a1c
SashkaHacker <am942081@gmail.com>
2021-04-17 21:46:03 +0300

    Взаимодействие с пользователем

--- tables/user.py
+++ tables/user.py
@@ -1,10 +1,11 @@
+from flask_login import UserMixin
 from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Text, Boolean
 from sqlalchemy.orm import validates, relationship
 from datetime import datetime
 from .db_session import SqlAlchemyBase
 
 
-class User(SqlAlchemyBase):
+class User(SqlAlchemyBase, UserMixin):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True, unique=True, autoincrement=True)
