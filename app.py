eb96bf343519ff6c4cada0702b01ed8f83a565f8
SashkaHacker <am942081@gmail.com>
2021-04-17 14:35:13 +0300

    пробую в html

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('main.html')
+    return render_template('page1.html')
 
 
 @app.route('/registration', methods=["POST", "GET"])
