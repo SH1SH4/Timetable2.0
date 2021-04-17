f31776909d926c873d155fe73040fcd30d410f01
SashkaHacker <am942081@gmail.com>
2021-04-17 21:46:03 +0300

    Взаимодействие с пользователем

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, url_for, request, redirect, abort
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 from forms.login import LoginForm
 from forms.register import RegisterForm
@@ -26,6 +26,14 @@ def index():
     return render_template('main.html', form=form)
 
 
+@app.route("/user")
+def user():
+    if current_user.is_authenticated:
+        return render_template("user_cabinet.html", title="Личный кабинет", user=current_user)
+    else:
+        abort(404)
+
+
 @app.route('/registration', methods=["POST", "GET"])
 def registration():
     form = RegisterForm()
@@ -38,13 +46,21 @@ def registration():
 
 @app.route("/login", methods=["POST", "GET"])
 def authorization():
+
     form = LoginForm()
     if request.method == "GET":
         return render_template('authorization.html', form=form)
     if request.method == "POST":
         if login(form.email.data, form.password.data):
             return redirect("/")
-        return "NOT OK"
+        return render_template('authorization.html', form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
 
 
 if __name__ == "__main__":
