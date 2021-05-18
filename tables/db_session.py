be84b310d5384be862ab815b18f87eb2d656baf3
SH1SH4 <dshisha0@gmail.com>
2021-05-18 15:43:11 +0300

    db_sess адекватная загрузка из env

--- tables/db_session.py
+++ tables/db_session.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from os import environ as env
 from os import path
-from dotenv import get_key
+from dotenv import load_dotenv
 
 DeclarativeBase = declarative_base()
 __factory = None
@@ -11,20 +11,13 @@ __factory = None
 dotenv_path = path.join(path.dirname(path.dirname(__file__)), '.env')
 print(dotenv_path)
 if path.exists(dotenv_path):
-    DRIVERNAME = get_key(dotenv_path, 'DB_DRIVERNAME')
-    HOST = get_key(dotenv_path, 'DB_HOST')
-    PORT = int(get_key(dotenv_path, 'DB_PORT'))
-    USERNAME = get_key(dotenv_path, 'USERNAME')
-    PASSWORD = get_key(dotenv_path, 'PASSWORD')
-    DATABASE = get_key(dotenv_path, 'DB')
-
-else:
-    DRIVERNAME = env.get('DB_DRIVERNAME')
-    HOST = env.get('DB_HOST')
-    PORT = int(env.get('DB_PORT'))
-    USERNAME = env.get('USERNAME')
-    PASSWORD = env.get('PASSWORD')
-    DATABASE = env.get('DB')
+    load_dotenv(dotenv_path)
+DRIVERNAME = env.get('DB_DRIVERNAME')
+HOST = env.get('DB_HOST')
+PORT = int(env.get('DB_PORT'))
+USERNAME = env.get('USERNAME')
+PASSWORD = env.get('PASSWORD')
+DATABASE = env.get('DB')
 
 
 def global_init():
